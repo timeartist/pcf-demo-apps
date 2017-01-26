4e73aded0847febf3350cdb6fac58e313f7ddc7f
Adi Foulger <technobabelfish@gmail.com>
2017-01-26 15:00:06 -0800

    make a little more comprehensive

--- web-benchmark/app.py
+++ web-benchmark/app.py
@@ -1,23 +1,43 @@
 from os import getenv
 from uuid import uuid4
+from json import loads
+from time import sleep
+import logging
 
 from redis import Redis, ConnectionError
 from flask import Flask
 
 DATA_SIZE = 1000
+SLEEP_INTERVAL = 1
+MAX_RETRY_TIMES = 30
 
 app = Flask(__name__)
+mode = ''
 
 
 @app.route('/')
-def test():
+def test(attempt=0):
     guid = uuid4()
-    redis.set(guid, 'x'*DATA_SIZE)
+
+    try:
+        redis.set(guid, 'x'*DATA_SIZE)
+    except:
+        if attempt == MAX_RETRY_TIMES:
+            raise
+
+        attempt += 1
+        sleep(SLEEP_INTERVAL)
+        test(attempt)
 
     return str(guid)
 
+@app.route('/mode')
+def mode():
+    return mode
+
 def make_redis():
     try:
+        global mode
         env = getenv('VCAP_SERVICES')
 
         if env is None:
@@ -25,22 +45,27 @@ def make_redis():
 
         service_env_vars = loads(env)['redislabs-enterprise-cluster'][0]
         credentials = service_env_vars['credentials']
-        dns = credentials['']
+        dns = credentials['host']
         ip = credentials['ip_list'][0]
         port = credentials['port']
         password = credentials['password']
 
         redis = Redis(dns, ip, port, password)
         redis.ping()
+        mode = 'dns'
+        logging.info('using dns: ' + dns)
         return redis
-    except ConnectionError:
+    except:
         try:
             redis = Redis(host=ip, port=port, password=password)
             redis.ping()
+            mode = 'ip'
+            logging.info('using ip: ' + ip)
             return redis
         except:
+            logging.info('using localhost')
             redis = Redis()
-            redis.ping()
+            mode = 'localhost'
             return redis
 
 redis = make_redis()
